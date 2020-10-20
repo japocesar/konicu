2ef0cbc82475ecd3fc6ca44bd71b745894319d2a
japocesar <cesar.carrasco.s@hotmail.com>
2020-10-20 16:12:08 -0500

    build

---
+++ docs/precache-manifest.e130dffbf822b2f410e7647b5d1e9422.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "55be96c4eb8ebe044c2d87c35cd9526a",
+    "url": "/konicu/index.html"
+  },
+  {
+    "revision": "22a9735290b457620f1f",
+    "url": "/konicu/static/css/2.2ce0d9b2.chunk.css"
+  },
+  {
+    "revision": "3a33671f0dd6a1332698",
+    "url": "/konicu/static/css/main.69dbcacb.chunk.css"
+  },
+  {
+    "revision": "22a9735290b457620f1f",
+    "url": "/konicu/static/js/2.d4fcc676.chunk.js"
+  },
+  {
+    "revision": "c789a8cceed6aa33e33ca6d7d1a6aca7",
+    "url": "/konicu/static/js/2.d4fcc676.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3a33671f0dd6a1332698",
+    "url": "/konicu/static/js/main.77e3c5a1.chunk.js"
+  },
+  {
+    "revision": "6ebcd5759b13a1f80b19",
+    "url": "/konicu/static/js/runtime-main.77350a17.js"
+  },
+  {
+    "revision": "89c7d244c77359e975b1781bd75eb0ea",
+    "url": "/konicu/static/media/01.89c7d244.jpg"
+  }
+]);
