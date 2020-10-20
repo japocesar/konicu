5c56702b09bb7b07a94f61b97fb436733ade1d61
japocesar <cesar.carrasco.s@hotmail.com>
2020-10-20 12:30:30 -0500

    docs

---
+++ docs/precache-manifest.d5cb868d5609aea4a2485ca11c3eab1f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e5a5a5f64d5db8fe7822573448338011",
+    "url": "/index.html"
+  },
+  {
+    "revision": "22a9735290b457620f1f",
+    "url": "/static/css/2.2ce0d9b2.chunk.css"
+  },
+  {
+    "revision": "82060cb6ae1f952c9239",
+    "url": "/static/css/main.795cb1cf.chunk.css"
+  },
+  {
+    "revision": "22a9735290b457620f1f",
+    "url": "/static/js/2.d4fcc676.chunk.js"
+  },
+  {
+    "revision": "c789a8cceed6aa33e33ca6d7d1a6aca7",
+    "url": "/static/js/2.d4fcc676.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "82060cb6ae1f952c9239",
+    "url": "/static/js/main.12eaa8a0.chunk.js"
+  },
+  {
+    "revision": "39c5f6ebad65753ea605",
+    "url": "/static/js/runtime-main.de3a236c.js"
+  }
+]);
