d690f7c32b8b535d1f5889e81198810dde68d938
japocesar <cesar.carrasco.s@hotmail.com>
2020-11-12 19:14:55 -0600

    fix overflow

--- src/data/bannerCircles.js
+++ src/data/bannerCircles.js
@@ -3,5 +3,5 @@ export const bannerCircles = [
     { texto: 'Aprende', color: 'pink', clase: 'animate__bounceInLeft', icono: faGraduationCap },
     { texto: 'Diviértete', color: 'orange', clase: 'animate__bounceInDown animate__delay-1s', icono: faLaughSquint },
     { texto: 'Colabora', color: 'green', clase: 'animate__bounceInDown animate__delay-2s', icono: faPeopleCarry },
-    { texto: 'Exprésate', color: 'blue', clase: 'animate__bounceInRight animate__delay-3s', icono: faComments },
+    { texto: 'Exprésate', color: 'blue', clase: 'animate__bounceInUp animate__delay-3s', icono: faComments },
 ]
