dac1ece3d6ada45bfb40b94aa95c50f3a805527f
japocesar <cesar.carrasco.s@hotmail.com>
2020-12-11 10:22:21 -0600

    fb share tags, topic card updates in home

--- src/components/ui/footer/Footer.js
+++ src/components/ui/footer/Footer.js
@@ -14,17 +14,20 @@ export const Footer = () => {
                 <div className="col-12 col-lg-4 offset-lg-8 d-flex flex-wrap p-3">
                     <div className="col-12 mb-2  text-center">Síguenos en nuestras redes sociales</div>
                     <div className="col-xs-12 col-4 text-center">
-                        <a className="text-white" href="https://www.facebook.com/ClubKonicu" title="Facebook" target="_blank">
+                        <a className="text-white" href="https://www.facebook.com/ClubKonicu" title="Facebook"
+                            target="_blank" rel="noopener noreferrer">
                             <FontAwesomeIcon icon={ faFacebookF } size="2x" />
                         </a>
                     </div>
                     <div className="col-xs-12 col-4 text-center">
-                        <a className="text-white" href="https://www.instagram.com/clubkonicu/" title="Instagram" target="_blank">
+                        <a className="text-white" href="https://www.instagram.com/clubkonicu/" title="Instagram" 
+                            target="_blank" rel="noopener noreferrer">
                             <FontAwesomeIcon icon={ faInstagram } size="2x" />
                         </a>
                     </div>
                     <div className="col-xs-12 col-4 text-center">
-                        <a className="text-white" href="https://open.spotify.com/show/2RErT6Z1u2xeT7B0AtZnqe" title="Spotify" target="_blank">
+                        <a className="text-white" href="https://open.spotify.com/show/2RErT6Z1u2xeT7B0AtZnqe" title="Spotify"
+                            target="_blank" rel="noopener noreferrer">
                             <FontAwesomeIcon icon={ faSpotify } size="2x" />
                         </a>
                     </div>
--- src/data/cards.js
+++ src/data/cards.js
@@ -1,6 +1,6 @@
-import { faSkull, faUserFriends, faSearch } from "@fortawesome/free-solid-svg-icons";
+import { faTree, faUserFriends, faSearch } from "@fortawesome/free-solid-svg-icons";
 export const cards = [
-    { titulo: 'Tema del Mes', descripcion: 'Una bonita tradición mexicana, conoce la historia así como los lugares más emblemáticos, así como los mejores para disfrutar de dicha celebración.', clase: 'card mx-auto mt-2', icono: faSkull, link: "/tema-del-mes" },
+    { titulo: 'Tema del Mes', descripcion: '¡Llegó la época de regalos, descanso y convivencia! Vívela con amor, esperanza, solidaridad, gratitud y nuevos propósitos', clase: 'card mx-auto mt-2', icono: faTree, link: "/tema-del-mes" },
     { titulo: 'Soy Su Fan', descripcion: 'Conoce las participaciones de los usuarios de Club Konicú', clase: 'card mx-auto mt-2', icono: faSearch, link: '/soy-su-fan' },
     { titulo: 'Exprésate', descripcion: 'Aporta tu experiencia, ideas o comentarios.', clase: 'card mx-auto mt-2', icono: faUserFriends, link: '/expresate' },
 ]
